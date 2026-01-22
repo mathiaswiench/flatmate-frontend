88ebcb8792b8e2f15e7508511f856f34068fd766
Matthias Wiench <47293500+mathiaswiench@users.noreply.github.com>
2026-01-22 22:01:06 +0100

    add env variables

--- src/api/calculateSettlement.tsx
+++ src/api/calculateSettlement.tsx
@@ -1,11 +1,16 @@
 import type { Settlement } from '../types';
 import type Flatmate from '../types';
 
+const isDev = import.meta.env.PUBLIC_DEV === 'true';
+const API_URL = isDev
+  ? import.meta.env.PUBLIC_LOCAL_API_URL
+  : import.meta.env.PUBLIC_API_URL;
+
 export const calculateSettlement = async (
   flamates: Flatmate[],
   totalDays: number,
 ): Promise<Settlement[]> => {
-  const response = await fetch('http://localhost:8080/settlement', {
+  const response = await fetch(`${API_URL}settlement`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
