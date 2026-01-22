0fef5ad471ac33a3f2e7137be40bb60f10b4aa1f
Matthias Wiench <47293500+mathiaswiench@users.noreply.github.com>
2026-01-22 21:17:08 +0100

    update config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,4 +4,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    // Please replace <REPO_NAME> with the repository name.
+    // For example, "/my-project/"
+    assetPrefix: '/flatmate-frontend/',
+  },
 });
